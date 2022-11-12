af800dd57cafd0027372ec533f8aa3d79a819375
Boomboomexploder <117991750+Boomboomexploder@users.noreply.github.com>
2022-11-12 18:27:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+function select(){
+    window.location="indexs.html";
+}
+ 
+function child_se(){
+    window.location="indexc.html";
+}
+
+function back1(){
+    window.location="indexs.html";
+}
+function back0(){
+    window.location="index.html";
+}
+function back2(){
+    window.location="indexs.html";
+}
+function teen_se(){
+    window.location="indext.html";
+}
+
+
+function adult_se(){
+    window.location="indexa.html";
+}
+
+function back3(){
+    window.location="indexs.html";
+}
+function senior_sel()
+{
+    window.location="indexo.html";
+}
+
+function back4()
+{
+    window.location="indexs.html";
+}
